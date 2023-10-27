0f8e3536eb8a828864e7b7bd10fba8ca065b4b19
jackr227 <149139596+jackr227@users.noreply.github.com>
2023-10-28 01:27:22 +0200

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,21 +1,23 @@
+// JavaScript code in "countdown.js"
 window.onload = function () {
     var countDownDate = new Date("Mar 27, 2024 18:00:00").getTime();
+    var countdownMessage = "Countdown to the Grand Opening";
 
     var x = setInterval(function () {
         var now = new Date().getTime();
         var distance = countDownDate - now;
 
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        var timer = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
-        document.getElementById("timer").innerHTML = timer;
-
         if (distance < 0) {
             clearInterval(x);
             document.getElementById("timer").innerHTML = "EXPIRED";
+        } else {
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            var timer = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+            document.getElementById("timer").innerHTML = timer;
         }
     }, 1000);
 };
