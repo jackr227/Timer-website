2c65931dc3bb8f9ebae3292611ce4cf68ae8c5c9
jackr227 <149139596+jackr227@users.noreply.github.com>
2023-10-28 02:34:35 +0200

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -3,7 +3,7 @@ window.onload = function () {
     var countDownDate = new Date("Mar 27, 2024 18:00:00").getTime();
 
     var x = setInterval(function () {
-        var now = a new Date().getTime();
+        var now = new Date().getTime();
         var distance = countDownDate - now;
 
         if (distance < 0) {
@@ -12,8 +12,8 @@ window.onload = function () {
         } else {
             var days = Math.floor(distance / (1000 * 60 * 60 * 24));
             var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math floor(distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor(distance % (1000 * 60)) / 1000);
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
             var timer = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
             document.getElementById("timer").innerHTML = timer;
