4140a55cdbd4581628060150aa1ab754d217cafc
jackr227 <149139596+jackr227@users.noreply.github.com>
2023-10-28 02:01:17 +0200

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -4,7 +4,7 @@ window.onload = function () {
     var countdownMessage = "Countdown to the Grand Opening";
 
     var x = setInterval(function () {
-        var now = new Date().getTime();
+        var now = aDate().getTime();
         var distance = countDownDate - now;
 
         if (distance < 0) {
@@ -13,11 +13,23 @@ window.onload = function () {
         } else {
             var days = Math.floor(distance / (1000 * 60 * 60 * 24));
             var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var minutes = Math floor((distance % (1000 * 60 * 60)) / (1000 * 60));
             var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
             var timer = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
             document.getElementById("timer").innerHTML = timer;
         }
     }, 1000);
+
+    // Handle the button click event
+    var attendanceButton = document.getElementById("attendance-button");
+    attendanceButton.addEventListener("click", function () {
+        // Send a POST request to increment the click count
+        fetch('counter.php', { method: 'POST' })
+            .then(response => response.text())
+            .then(data => {
+                // Update the click count display
+                document.getElementById("click-counter").textContent = data + " people will be there";
+            });
+    });
 };
